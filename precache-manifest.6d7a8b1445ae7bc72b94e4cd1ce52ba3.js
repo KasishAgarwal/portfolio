cde5d206e88880ec3b4762b122c5d4d54e1117c1
Kasish Agarwal <kasishagarwal9@gmail.com>
2020-09-09 12:50:07 +0530

    Updates

---
+++ precache-manifest.6d7a8b1445ae7bc72b94e4cd1ce52ba3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f267cce9b45a7a9f6d31290baec46e8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b0c7bb41356134a75800",
+    "url": "/portfolio/static/css/main.47f6435e.chunk.css"
+  },
+  {
+    "revision": "5b6354854aa418bd22a1",
+    "url": "/portfolio/static/js/2.b7345c27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.b7345c27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0c7bb41356134a75800",
+    "url": "/portfolio/static/js/main.d40b4b45.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  }
+]);
